a2df4bbdb99c43cb7ffa9ca7d55cd02c8cbae3a6
Murali Venkat <muralivenkat0122@gmail.com>
2016-10-17 12:53:27 -0500

    Person Class Implemented...!

---
+++ HackerRank/30DaysOfCoding/Day12/day12hacker/Inheritance/Person.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package day12hacker.Inheritance;
+
+/**
+ * @author Murali Venkat 
+ *
+ */
+public class Person {
+	protected String firstName;
+	protected String lastName;
+	protected int idNumber;
+	
+	// Constructor
+	Person(String firstName, String lastName, int identification){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = identification;
+	}
+	
+	// Print person data
+	public void printPerson(){
+		 System.out.println(
+				"Name: " + lastName + ", " + firstName 
+			+ 	"\nID: " + idNumber); 
+	}
+}
