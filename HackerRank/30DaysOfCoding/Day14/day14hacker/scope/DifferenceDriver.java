69667087b61e0abbdba9c0ae68332417dc13d690
Murali Venkat <muralivenkat0122@gmail.com>
2016-10-20 01:02:47 -0500

    Main Driver Class Commit

---
+++ HackerRank/30DaysOfCoding/Day14/day14hacker/scope/DifferenceDriver.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package day14hacker.scope;
+
+import java.util.Scanner;
+
+/**
+ * @author Murali Venkat
+ *
+ */
+public class DifferenceDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = sc.nextInt();
+		}
+		sc.close();
+
+		Difference difference = new Difference(a);
+
+		difference.computeDifference();
+
+		System.out.print(difference.maximumDifference);
+	}
+
+}
