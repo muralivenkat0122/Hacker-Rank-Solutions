460d3c07024a1241f19aa51781d5e3f3886cd7a4
Murali Venkat <muralivenkat0122@gmail.com>
2016-10-21 02:02:38 -0500

    Node Class Implemented

---
+++ HackerRank/30DaysOfCoding/Day15/day15hacker/linkedlist/Node.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package day15hacker.linkedlist;
+
+/**
+ * @author Murali Venkat 
+ *
+ */
+public class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
