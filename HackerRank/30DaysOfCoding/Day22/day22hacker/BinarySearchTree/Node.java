a5f00c96e17bacb91305c9a6305d3f061d030433
Murali Venkat <muralivenkat0122@gmail.com>
2016-10-28 20:09:31 -0500

    Node Class Commit

---
+++ HackerRank/30DaysOfCoding/Day22/day22hacker/BinarySearchTree/Node.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package day22hacker.BinarySearchTree;
+
+/**
+ * @author Murali Venkat 
+ *
+ */
+public class Node {
+	   Node left,right;
+	    int data;
+	    Node(int data){
+	        this.data=data;
+	        left=right=null;
+	    }
+}
