eae30235e483157ddcf5b8948b3720abe50af04a
Murali Venkat <muralivenkat0122@gmail.com>
2016-10-30 17:46:10 -0500

    Day 24 Node Class Implementation

---
+++ HackerRank/30DaysOfCoding/Day24/day24hacker/MoreLinkedLists/Node.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package day24hacker.MoreLinkedLists;
+
+/**
+ * @author Murali Venkat 
+ *
+ */
+public class Node {
+	int data;
+	Node next;
+	Node(int d){
+        data=d;
+        next=null;
+    }
+}
