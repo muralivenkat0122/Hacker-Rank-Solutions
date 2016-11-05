30a83b0c1283e2436dff2ea9e8f97fea4e58966b
Murali Venkat <muralivenkat0122@gmail.com>
2016-11-05 18:55:19 -0500

    Day 28 Hacker Rank Solution 2 Details.java

---
+++ HackerRank/30DaysOfCoding/Day28/day28hacker/Solution2/Details.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package day28hacker.Solution2;
+
+/**
+ * @author Murali Venkat 
+ *
+ */
+public class Details {
+	String name;
+	String email;
+	
+	/**
+	 * 
+	 */
+	public Details() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Details(String name, String email){
+		this.name = name;
+		this.email = email;
+	}
+	
+	
+	
+	
+}
