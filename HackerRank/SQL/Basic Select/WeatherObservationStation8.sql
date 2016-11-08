68561a8b13f76d2196b2b4ad3ac4b2ea1cd80775
Murali Venkat <muralivenkat0122@gmail.com>
2016-11-07 19:21:49 -0600

    Weather Observation Station 8 SQL Select Statements Commit

---
+++ HackerRank/SQL/Basic Select/WeatherObservationStation8.sql
@@ -0,0 +1,28 @@
+/*
+ * Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, 
+ * and u) as both their first and last characters. Your result cannot contain 
+ * duplicates.
+==========================================================================================
+  
+Input Format
+
+The STATION table is described as follows:
+				
+							STATION
+				================================
+					Field			Type
+				================================	
+					ID				NUMBER
+					CITY			VARCHAR2(21)
+					STATE			VARCHAR2(2)
+					LAT_N			NUMBER
+					LAT_W			NUMBER
+				================================
+where LAT_N is the northern latitude and LONG_W is the western longitude.				
+==========================================================================================
+*/ 
+
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LCASE(CITY) REGEXP "^[a,e,i,o,u].*[a,e,i,o,u]$";
