89d9ee08b6274f3af95834dff2ced841671208b4
Murali Venkat <muralivenkat0122@gmail.com>
2016-11-11 17:30:58 -0600

    Types Of Triangle Advanced Select Statements Updated

---
+++ HackerRank/SQL/Advanced Select/TypesOfTriangle.sql
@@ -0,0 +1,71 @@
+/*
+ * Write a query identifying the type of each record in the TRIANGLES table using its 
+ * three side lengths. Output one of the following statements for each record in the 
+ * table:
+
+	Not A Triangle: The given values of A, B, and C don't form a triangle.
+	Equilateral: It's a triangle with 3 sides of equal length.
+	Isosceles: It's a triangle with 2 sides of equal length.
+	Scalene: It's a triangle with 3 sides of differing lengths.
+============================================================================================
+	
+Input Format
+
+The TRIANGLES table is described as follows:
+
+						=================================
+							Column			Type
+						=================================
+							A				Integer
+							B				Integer
+							C				Integer
+						=================================	
+
+Each row in the table denotes the lengths of each of a triangle's three sides.
+============================================================================================
+
+Sample Input
+						
+						=================================
+							A			B			C
+						=================================
+							20			20			23
+							20			20			20
+							20			21			22
+							13			14			30
+						=================================
+============================================================================================
+								
+Sample Output
+
+	Isosceles
+	Equilateral
+	Scalene
+	Not A Triangle
+============================================================================================
+	
+Explanation
+
+	Values in the tuple (20,20,23) form an Isosceles triangle, because A = B OR B = C or 
+	C = A. 
+	Values in the tuple (20,20,20) form an Equilateral triangle, because A = B AND 
+	B = C. 
+	Values in the tuple (20,21,22) form a Scalene triangle, because A != B != C. 
+	Values in the tuple (13,14,30) cannot form a triangle because the combined value 
+	of sides A and B is not larger than that of side C.
+============================================================================================
+	
+*/
+
+
+
+SELECT CASE 
+            WHEN A+B > C AND B+C > A AND A+C > B THEN
+                    CASE
+                          WHEN A = B AND B = C THEN 'Equilateral'
+                          WHEN A = B OR B = C OR C = A THEN 'Isosceles'
+                          ELSE 'Scalene'
+                    END
+            ELSE 'Not A Triangle'
+        END
+FROM TRIANGLES;
